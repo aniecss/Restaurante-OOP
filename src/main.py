9824aa036cb2125178d6f4efe2ef109a6c4ae335
aniecss <kariohana10@gmail.com>
2025-05-12 17:53:14 -0300

    # avaliacao.py
    # Classe Avaliacao para representar a nota e o cliente que avaliou um restaurante.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from restaurante import Restaurante
+
+def main():
+    restaurante1 = Restaurante('Praça', 'Gourmet')
+    restaurante1.receber_avaliacao('Ana', 5)
+    restaurante1.receber_avaliacao('Maria', 4)
+    restaurante1.receber_avaliacao('Júlia', 3)
+
+    print('\n--- Lista de Restaurantes ---\n')
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
